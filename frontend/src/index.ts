45ed750b719b0af3adb8f819885e400477d18c40
Antônio Augusto Duarte <aadm.aquino@gmail.com>
2025-01-19 22:35:44 -0300

    Add Carousel's actions in frontend

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,8 @@
 import './style.css';
-import { CarouselsAPI } from './api/api.js';
-import { getConfiguration, initializeColorPicker } from './settings';
+import { CarouselsAPI } from './api/api';
+import { Carousel } from './interfaces/carousel';
+import { Slide } from './interfaces/slide';
+import { getConfiguration, getFormattedDate, initializeColorPicker } from './settings';
 import { splitText } from './utils/split-text';
 import { CarouselSlide } from './carousel-slide';
 import { createListItems } from './utils/create-list-items';
@@ -21,14 +23,79 @@ initializeColorPicker({
     backgroundColorPreview: document.getElementById("backgroundColorPreview") as HTMLElement
 });
 
+async function listCarousels() {
+    try {
+        displayText.innerHTML = "";
+        const carousels: Carousel[] = await CarouselsAPI.getAll();
+
+        carousels.forEach(carousel => {
+            const slide_text: string[] = carousel.slides
+                .sort((a, b) => a.position - b.position)
+                .map(slide => slide.quill_delta_content);
+
+            const listItems = createListItems(slide_text, {
+                fontColor: carousel.slides[0]?.font_color || "#000000ff",
+                backgroundColor: carousel.slides[0]?.background_color || "#000000ff"
+            });
+
+            displayText.append(listItems);
+        });
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+    }
+}
+
+async function getCarouselById(id: number) {
+    try {
+        displayText.innerHTML = "";
+        const carousel: Carousel = await CarouselsAPI.getById(id);
+
+        const slide_text: string[] = carousel.slides
+            .sort((a, b) => a.position - b.position)
+            .map(slide => slide.quill_delta_content);
+
+        const listItems = createListItems(slide_text, {
+            fontColor: carousel.slides[0]?.font_color || "#000000ff",
+            backgroundColor: carousel.slides[0]?.background_color || "#000000ff"
+        });
+
+        displayText.append(listItems);
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+    }
+}
+
+async function createCarousel(carousel: Carousel) {
+    try {
+        displayText.innerHTML = "";
+        console.log(carousel);
+        const data: Carousel = await CarouselsAPI.create(carousel);
+
+        const slide_text: string[] = data.slides
+            .sort((a, b) => a.position - b.position)
+            .map(slide => slide.quill_delta_content);
+
+        const listItems = createListItems(slide_text, {
+            fontColor: carousel.slides[0]?.font_color || "#000000ff",
+            backgroundColor: carousel.slides[0]?.background_color || "#000000ff"
+        });
+
+        displayText.append(listItems);
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+    }
+}
+
 const textInput = document.getElementById("textInput")! as HTMLInputElement;
 const submitButton = document.getElementById("submitButton")!;
+const listCarouselsButton = document.getElementById("listCarouselsButton")!;
+const carouselIdInput = document.getElementById("carouselIdInput")! as HTMLInputElement;
+const getCarouselButton = document.getElementById("getCarouselButton")!;
 export const displayText = document.getElementById("displayText")!;
 const charCounter = document.getElementById("charCounter")!;
 const errorMessage = document.getElementById("errorMessage")!;
 
 submitButton.addEventListener("click", () => {
-    CarouselsAPI.getAll().then(data => console.log(data));
     const text = textInput.value;
     errorMessage.textContent = "";
 
@@ -44,7 +111,39 @@ submitButton.addEventListener("click", () => {
         displayText.innerHTML = '';
         const listItems = createListItems(splitItems, { fontColor, backgroundColor });
         displayText.append(listItems);
+
+        const slides: Slide[] = splitItems.map((item, index) => ({
+            background_color: backgroundColor,
+            font_color: fontColor,
+            position: index + 1,
+            quill_delta_content: item
+        }));
+
+        const carousel: Carousel = {
+            name: "Carousel " + getFormattedDate(),
+            is_active: true,
+            slides: slides
+        };
+
+        createCarousel(carousel);
     }
+
+    return;
+});
+
+listCarouselsButton.addEventListener("click", () => {
+    listCarousels();
+
+    return;
+});
+
+getCarouselButton.addEventListener("click", () => {
+    const carouselId = parseInt(carouselIdInput.value);
+    if (isNaN(carouselId)) {
+        alert("Please, type a valid ID.");
+        return;
+    }
+    getCarouselById(carouselId);
 
     return;
 });
@@ -53,3 +152,8 @@ textInput.addEventListener("input", () => {
     charCounter.textContent = `Characters: ${textInput.value.length}`;
 });
 
+document.addEventListener("DOMContentLoaded", async () => {
+    listCarousels();
+
+    return;
+});
